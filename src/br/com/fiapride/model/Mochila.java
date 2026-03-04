762aa546956d811d1c07f411efb8f526185fa2e0
Gabriel Nacarelli <gabriel.nacarelli@gmail.com>
2026-03-04 14:06:43 -0300

    feat: adiciona métodos de comportamento e atualiza README

--- src/br/com/fiapride/model/Mochila.java
+++ src/br/com/fiapride/model/Mochila.java
@@ -1,8 +1,41 @@
 package br.com.fiapride.model;
-public class Mochila { // <-- Use o nome do SEU objeto    
-    // As características que você pensou    
+
+public class Mochila {
     public String cor;
     public String material;
     public int capacidadeEmKg;
-    
+    public double pesoAtual;
+
+    public Mochila(String cor, String material, int capacidadeEmKg) {
+        this.cor = cor;
+        this.material = material;
+        this.capacidadeEmKg = capacidadeEmKg;
+        this.pesoAtual = 0.0;
+    }
+
+    public void guardarItem(double pesoItem) {
+        if (pesoItem <= 0) {
+            System.out.println("Erro: O peso do item deve ser maior que zero.");
+            return;
+        }
+        if (this.pesoAtual + pesoItem > this.capacidadeEmKg) {
+            System.out.println("Erro: Capacidade excedida! A mochila não suporta este item.");
+            return;
+        }
+        this.pesoAtual += pesoItem;
+        System.out.println("Item guardado com sucesso. Peso atual: " + this.pesoAtual + "Kg");
+    }
+
+    public void retirarItem(double pesoItem) {
+        if (pesoItem <= 0) {
+            System.out.println("Erro: O peso a ser retirado deve ser maior que zero.");
+            return;
+        }
+        if (this.pesoAtual - pesoItem < 0) {
+            System.out.println("Erro: Não há peso suficiente na mochila para ser retirado.");
+            return;
+        }
+        this.pesoAtual -= pesoItem;
+        System.out.println("Item retirado com sucesso. Peso atual: " + this.pesoAtual + "Kg");
+    }
 }
